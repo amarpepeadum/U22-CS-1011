3c862c677df72523715983cb41e2238a92491559
amarpepeadum <54312907+amarpepeadum@users.noreply.github.com>
2024-01-25 22:10:37 +0100

    Add files via upload

--- MainCalculator.java
+++ MainCalculator.java
@@ -1,5 +1,52 @@
-class Maincalculator{
+import java.util.Scanner;
+
+public class MainCalculator{
     public static void main(String[] args){
+
+        double firstNumber, secondNumber, result;
+
+        char operator;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+
+        firstNumber = scanner.nextDouble();
+
+        System.out.print("Input second number: ");
+
+        secondNumber = scanner.nextDouble();
+
+        System.out.print("Input your operator: ");
+
+        operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                System.out.println(result);
+                break;
+            
+            case '-':
+                result = firstNumber - secondNumber;
+                System.out.println(result);
+                break;
+                
+            case '*':
+                result = firstNumber * secondNumber;
+                System.out.println(result);
+                break;
+
+            case '/':
+                result = firstNumber / secondNumber;
+                System.out.println(result);
+                break;
+
+            default:
+            System.out.println("You don do mistake boss!");
+                break;
+        }
+
 
     }
 }
